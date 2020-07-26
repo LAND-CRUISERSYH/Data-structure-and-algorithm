a4c850ae063aac18b90c521eb90daca65b8ab831
LAND-CRUISERSYH <1354843647@qq.com>
2020-07-26 19:23:20 +0800

    顺序表

--- 顺序表/顺序表/test.c
+++ 顺序表/顺序表/test.c
@@ -1,7 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>                //malloc()、free()函数原型
+
+#define LIST_SIZE 20           //顺序表中可存放元素个数
+
+typedef int Datatype;
+
+typedef struct {
+	Datatype *pL;
+	int length;                    //顺序表元素个数(表长)
+}SqList;
+
+void InitList(SqList *p);              //初始化顺序表
+
 
 int main()
 {
-	printf("hello world");
-	return 0;
+	//SqList L;                   //创建一个顺序表
+	return 0; 
+}
+
+void InitList(SqList *p)
+{
+	p->pL = (Datatype*)malloc(LIST_SIZE * sizeof(Datatype));    //动态分配内存
+	if (p->pL == NULL)
+		exit(1);                               //内存分配失败
+	p->length = 0;                  //表长为0；
 }
