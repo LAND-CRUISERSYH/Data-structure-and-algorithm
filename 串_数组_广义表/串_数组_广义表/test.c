5bd7d3c11da61f4ed64e4df4ee0ed7c26e2c460b
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-03 22:15:36 +0800

    串的类型定义

--- 串_数组_广义表/串_数组_广义表/test.c
+++ 串_数组_广义表/串_数组_广义表/test.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
+#include<string.h>
+
+//#define SIZE 20
+//
+//typedef struct
+//{
+//	char ch[SIZE + 1];                 //存储串的一维数组
+//	int length;							//串的当前长度
+//}str;
+
+#define CHUNKSIZE 20
+
+typedef struct Chunk
+{
+	char ch[CHUNKSIZE];                 //存放一个块中的字符
+	struct Chunk *next;                    //指向下一个块
+}Chunk;
+
+typedef struct
+{
+	Chunk *head, *tail;                    //串的头指针和尾指针
+	int length;                                 //串的当前长度
+}LStr;                                           //字符串的块链结构
 
 int main()
 {
-	printf("hello world");
+	
 	return 0;
 }
