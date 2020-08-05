5a33db6564d99d48ab15241d9a1b6fd4225f3436
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-06 03:26:10 +0800

    树和二叉树

--- 树和二叉树/树和二叉树/test.c
+++ 树和二叉树/树和二叉树/test.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 
+//#define MAXSIZE 100                                    //二叉树顺序定义
+//
+//typedef int Datatype;
+//
+//typedef Datatype SqBiTree[MAXSIZE];
+
+//typedef int Datatype;                                   //二叉链表
+//
+//typedef struct BiNode {
+//	Datatype data;
+//	struct BiNode *lchild, *rchild;                //左右孩子指针
+//}BiNode,*BiTree;
+
+typedef int Datatype;
+
+typedef struct TriTNode
+{
+	Datatype data;
+	struct TriTNode *lchild, *parent, *rchild;                        //左孩子、双亲及右孩子指针
+}TriTNode,*TriTree;
+
 int main()
 {
-	printf("hello world");
 	return 0;
 }
