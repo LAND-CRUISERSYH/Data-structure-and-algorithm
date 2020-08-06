ce85675508af12c854040793046cbfbcf9aebe17
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-06 23:28:34 +0800

    遍历二叉树

--- 遍历二叉树和线索二叉树/遍历二叉树和线索二叉树/test.c
+++ 遍历二叉树和线索二叉树/遍历二叉树和线索二叉树/test.c
@@ -12,6 +12,7 @@ typedef struct BiNode
 void PreOrderTraverse(BiTree T);            //先序遍历
 void InOrderTraverse(BiTree T);              //中序遍历
 void PostOrderTraverse(BiTree T);         //后序遍历
+void CreateBiTree(BiTree *T);                 //二叉树的建立
 
 
 int main()
@@ -44,4 +45,20 @@ void PostOrderTraverse(BiTree T)         //后序遍历
 	PostOrderTraverse(T->lchild);
 	PostOrderTraverse(T->rchild);
 	printf("%c ", T->data);
+}
+
+void CreateBiTree(BiTree *T)                 //二叉树的建立
+{
+	char ch;
+	scanf("%c", &ch);
+	if (ch == '#')
+		*T = NULL;
+	else
+	{
+		if (!(*T = (BiNode *)malloc(sizeof(BiNode))))
+			return;
+		(*T)->data = ch;            //生成根结点
+		CreateBiTree((*T)->lchild);   //构造左子树
+		CreateBiTree((*T)->rchild);  //构造右子树
+	}
 }
