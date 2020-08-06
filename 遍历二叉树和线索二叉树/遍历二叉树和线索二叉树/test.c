7c67efb461db21dfa1f3ad32abb74d20f56ca556
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-07 03:36:33 +0800

    遍历二叉树和线索二叉树

--- 遍历二叉树和线索二叉树/遍历二叉树和线索二叉树/test.c
+++ 遍历二叉树和线索二叉树/遍历二叉树和线索二叉树/test.c
@@ -9,16 +9,22 @@ typedef struct BiNode
 	struct BiNode *lchild, *rchild;					//左右孩子指针
 }BiNode, *BiTree;
 
+typedef struct BiThrNode
+{
+	Datatype data;
+	int ltag, rtag;
+	struct BiThrNode *lchild, *rchild;
+}BiThrNode,*BiThrTree;
+
 void PreOrderTraverse(BiTree T);            //先序遍历
 void InOrderTraverse(BiTree T);              //中序遍历
 void PostOrderTraverse(BiTree T);         //后序遍历
 void CreateBiTree(BiTree *T);                 //二叉树的建立
+void Copy(BiTree T, BiTree *NewT);         //二叉树的复制
+int Depth(BiTree T);                                //求二叉树深度
+int NodeCount(BiTree T);                       //求二叉树结点总数
+int LeadCount(BiTree T);                        //求二叉树叶子结点数
 
-
-int main()
-{
-
-}
 
 void PreOrderTraverse(BiTree T)            //先序遍历
 {
@@ -52,13 +58,66 @@ void CreateBiTree(BiTree *T)                 //二叉树的建立
 	char ch;
 	scanf("%c", &ch);
 	if (ch == '#')
-		*T = NULL;
+		(*T) = NULL;
 	else
 	{
-		if (!(*T = (BiNode *)malloc(sizeof(BiNode))))
+		if (!((*T) = (BiNode *)malloc(sizeof(BiNode))))
 			return;
 		(*T)->data = ch;            //生成根结点
-		CreateBiTree((*T)->lchild);   //构造左子树
-		CreateBiTree((*T)->rchild);  //构造右子树
+		CreateBiTree(&(*T)->lchild);   //构造左子树
+		CreateBiTree(&(*T)->rchild);  //构造右子树
 	}
+}
+
+void Copy(BiTree T, BiTree *NewT)         //二叉树的复制
+{
+	if (T == NULL)
+	{
+		(*NewT) == NULL;
+		return;
+	}
+	else
+	{
+		(*NewT) = (BiTree)malloc(sizeof(BiNode));
+		if (!(*NewT))
+		{
+			printf("分配失败!\n");
+			return;
+		}
+		(*NewT)->data = T->data;
+		Copy(T->lchild, &(*NewT)->lchild);
+		Copy(T->rchild, &(*NewT)->rchild);
+	}
+}
+
+int Depth(BiTree T)                                //求二叉树深度
+{
+	if (T == NULL)
+		return 0;
+	else
+	{
+		int m = Depth(T->lchild);
+		int n = Depth(T->rchild);
+		return (m > n) ? (m + 1) : (n + 1);
+	}
+}
+
+int NodeCount(BiTree T)                       //求二叉树结点总数
+{
+	if (T == NULL)
+		return 0;
+	else
+	{
+		return NodeCount(T->lchild) + NodeCount(T->rchild) + 1;
+	}
+}
+
+int LeadCount(BiTree T)                        //求二叉树叶子结点数
+{
+	if (T == NULL)
+		return 0;
+	if (T->lchild == NULL && T->rchild == NULL)
+		return 1;
+	else
+		return LeadCount(T->lchild) + LeadCount(T->rchild);
 }
