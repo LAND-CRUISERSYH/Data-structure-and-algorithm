b0ce71255b1f3fce3be4277cadc5fad17eb74e05
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-10 02:59:43 +0800

    图的邻接矩阵表示法

--- 图的存储结构/图的存储结构/test.c
+++ 图的存储结构/图的存储结构/test.c
@@ -1,31 +1,56 @@
 #include<stdio.h>
 
-#define list 3
-#define line 3
+#define MVNum 100                                //最大顶点数     
+#define MaxInt 32767                            //表示极大值，即∞
 
-void Judge(char board[list][line], char *input, int t, int e)
+typedef char VerTexType;                         //设顶点的数据类型为字符型
+typedef int ArcType;								//假设边的权值类型为整型
+typedef struct
 {
-	int a = 0;
-	int b = 0;
-	int i = 0;
-	for (a = 0; a < t; a++)
+	VerTexType vexs[MVNum];                 //顶点表
+	ArcType arcs[MVNum][MVNum];       //邻接矩阵
+	int vexnum, arcnum;                        //图的当前顶点数和边数
+}AMGraph;
+
+int LocateVex(AMGraph *G, VerTexType u)    //在图G中查找顶点u，存在则返回顶点表中的下标，否则返回-1
+{
+	for (int i = 0; i < G->vexnum; i++)
 	{
-		for (b = 0; b < e; b++)
-		{
-			if (board[a][b] != ' ')
-				i++;
-		}
+		if (u == G->vexs[i])
+			return i;
 	}
-	if (i = t * e)
-		*input = 'T';
-	else
-		*input = 'F';
+	return -1;
 }
 
-int main()
+void CreateUDN(AMGraph *G)                        //采用邻接矩阵表示法创建无向网
 {
-	char arr[list][line] = { ' ',' ',' ',' ',' ',' ',' ',' ',' ' };
-	char ch;
-	Judge(arr, &ch, list,line);
-	printf("是否平局: %c\n", ch);
+	printf("请输入顶点数: ");                                //输入顶点数
+	scanf("%d", &G->vexnum);
+	printf("请输入边数: ");                                  //输入总边数
+	scanf("%d", &G->arcnum);
+	for (int i = 0; i < G->vexnum; i++)
+	{
+		scanf("%c", &G->vexs[i]);                      //依次输入点的信息
+	}
+	for (int i = 0; i < G->vexnum; i++)
+	{
+		for (int j = 0; j < G->vexnum; j++)
+		{
+			G->arcs[i][j] = MaxInt;                      //边的权值均置为极大值
+		}
+	}
+	for (int k = 0; i < G->arcnum; k++)         //构造邻接矩阵
+	{
+		VerTexType v1, v2;              
+		ArcType w;
+		printf("请输入一条边所依附的两个顶点: ");
+		scanf("%c %c", &v1, &v2);                               //输入一条边所依附的顶点
+		printf("\n请输入边的权值: ");
+		scanf("%d", &w);                                      //输入边的权值
+		putchar('\n');
+		int i = LocateVex(G, v1);
+		int j = LocateVex(G, v2);                              //确定v1和v2在G中的位置
+		G->arcs[i][j] = w;                                         //边的权值置为w
+		G->arcs[j][i] = G->arcs[i][j];                         //对应对称权值也设为w
+	}
 }
