17094982baf3ad1ae041e61fe4a0ddde09f439d9
LAND-CRUISERSYH <1354843647@qq.com>
2020-08-19 02:36:25 +0800

    排序

--- 排序/排序/test.c
+++ 排序/排序/test.c
@@ -73,41 +73,41 @@ typedef struct                                        //定义顺序表的结构
 //		ShellInsert(L, dlta[k]);                             //一趟增量为dlta[k]的插入排序
 //}
 
-void Bubble_Sort(SqList *L)                           //冒泡排序
-{
-	for (int i = 1; i <= L->length - 1; i++)                 //总共需length-1趟
-	{
-		for (int j = 1; j <= L->length - i; j++)
-		{
-			if (L->r[j + 1].key < L->r[j].key)                      //发生逆序
-			{
-				L->r[0].key = L->r[j].key;                          //交换
-				L->r[j].key = L->r[j + 1].key;
-				L->r[j + 1].key = L->r[0].key;
-			}
-		}
-	}
-}
-
-/*一旦某一趟比较时不出现记录交换，说明已经排好序了*/
-void Bubble_Sort2(SqList *L)                           //冒泡排序算法改进
-{
-	int flag = 1;                         //flag作为是否有交换的标记
-	for (int i = 1; i <= L->length - 1&&flag==1; i++)                 //总共需length-1趟
-	{
-	    flag = 0;
-		for (int j = 1; j <= L->length - i; j++)
-		{
-			if (L->r[j + 1].key < L->r[j].key)                      //发生逆序
-			{
-				L->r[0].key = L->r[j].key;                          //交换
-				L->r[j].key = L->r[j + 1].key;
-				L->r[j + 1].key = L->r[0].key;
-				flag = 1;                               //发生交换，flag置为1，若本趟没发生交换，flag保持为0
-			}
-		}
-	}
-}
+//void Bubble_Sort(SqList *L)                           //冒泡排序
+//{
+//	for (int i = 1; i <= L->length - 1; i++)                 //总共需length-1趟
+//	{
+//		for (int j = 1; j <= L->length - i; j++)
+//		{
+//			if (L->r[j + 1].key < L->r[j].key)                      //发生逆序
+//			{
+//				L->r[0].key = L->r[j].key;                          //交换
+//				L->r[j].key = L->r[j + 1].key;
+//				L->r[j + 1].key = L->r[0].key;
+//			}
+//		}
+//	}
+//}
+//
+///*一旦某一趟比较时不出现记录交换，说明已经排好序了*/
+//void Bubble_Sort2(SqList *L)                           //冒泡排序算法改进
+//{
+//	int flag = 1;                         //flag作为是否有交换的标记
+//	for (int i = 1; i <= L->length - 1&&flag==1; i++)                 //总共需length-1趟
+//	{
+//	    flag = 0;
+//		for (int j = 1; j <= L->length - i; j++)
+//		{
+//			if (L->r[j + 1].key < L->r[j].key)                      //发生逆序
+//			{
+//				L->r[0].key = L->r[j].key;                          //交换
+//				L->r[j].key = L->r[j + 1].key;
+//				L->r[j + 1].key = L->r[0].key;
+//				flag = 1;                               //发生交换，flag置为1，若本趟没发生交换，flag保持为0
+//			}
+//		}
+//	}
+//}
 
 //void sort(int *arr,int length)
 //{
@@ -126,6 +126,53 @@ void Bubble_Sort2(SqList *L)                           //冒泡排序算法改进
 //	}
 //}
 
+//int Partition(SqList *L, int low, int high)
+//{
+//	L->r[0] = L->r[low];
+//	KeyType pivotkey = L->r[low].key;
+//	while (low < high)
+//	{
+//		while (low < high&&L->r[high].key >= pivotkey)
+//			high--;
+//		L->r[low] = L->r[high];
+//		while (low < high&&L->r[low].key <= pivotkey)
+//			low++;
+//		L->r[high] = L->r[low];
+//	}
+//	L->r[low] = L->r[0];
+//	return low;
+//}
+//
+//
+//void QSort(SqList *L, int low, int high)             //对顺序表L快速排序
+//{
+//	if (low < high)                                            //长度大于1
+//	{
+//		int pivotloc = Partition(L, low, high);   //将L->r[low...high]一分为二，pivotloc为中心元素排好序的位置
+//		QSort(L, low, pivotloc - 1);                     //对低子表递归排序
+//		QSort(L, pivotloc + 1, high);                   //对高子表递归排序
+//	}
+//}
+
+void SelectSort(SqList *L)                                //简单选择排序
+{
+	for (int i = 1; i < L->length; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j <= L->length; j++)
+		{
+			if (L->r[j].key < L->r[min].key)
+				min = j;                 //记录最小值位置
+		}
+		if (min != i)
+		{
+			L->r[0] = L->r[min];         //交换
+			L->r[min] = L->r[i];
+			L->r[i] = L->r[0];
+		}
+	}
+}
+
 int main()
 {
 	SqList L;
@@ -139,7 +186,8 @@ int main()
 	//BinsertSort(&L);
 	//ShellSort(&L, arr, 3);
 	//Bubble_Sort(&L);
-
+	//QSort(&L, 1, 10);
+	SelectSort(&L);
 	for (int i = 1; i <= MAXSIZE; i++)
 	{
 		printf("%d ", L.r[i].key);
